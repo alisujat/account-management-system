2d60ac19bfa9c0dc15d7341625da66a0fcec1a5f
Sujat Ali <sujat.ali@innobitsystems.com>
2022-09-30 14:32:36 +0530

    ...

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 require("./conn/conn");
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -10,8 +11,6 @@ app.use(bodyParser.json())
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-// const validator = require("./validators/validate");
-// app.use("/bankStatementSubmission",validator);
 
 const bankStatementRouter = require("./routes/route");
 app.use('/bankStatement' , bankStatementRouter);
